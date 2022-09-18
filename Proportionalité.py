61a1f04bf365e065e1c92d33cee8f3db45598cac
Donald2304 <106539903+Donald2304@users.noreply.github.com>
2022-09-18 10:29:47 +0200

    Add files via upload

---
+++ Proportionalité.py
@@ -0,0 +1,23 @@
+running = True
+def tableau():
+    print('|| || ||')
+    print('|| || ||')
+
+def ChoixCaseTableau():
+    Choix = input("Choix Case ->")
+    if Choix == '1':
+        print("1")
+    elif Choix == '2':
+        print("2")
+    elif Choix == '3':
+        print("3")
+    elif Choix == '4':
+        print("4")
+    elif Choix == '5':
+        print("5")
+    elif Choix == '6':
+        print("6")
+
+
+while running:
+    ...
