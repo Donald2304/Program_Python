2eac1efc9b57772b21f33ba219587eda71a4fdc2
Donald2304 <106539903+Donald2304@users.noreply.github.com>
2022-10-01 08:32:35 +0200

    Update PointAligne.py

--- PointAligne.py
+++ PointAligne.py
@@ -1,11 +1,41 @@
+class Couleur:
+    Bleu = '\033[94m' 
+    Rouge = '\033[91m' 
+    Normal = '\033[0m'
+    Blanc = '\033[97m' 
+    Noire = '\033[30m'
+    Vert = '\033[32m'
+    Orange = '\033[33m'
+    Jaune = '\033[93m'
+    Violet = '\033[95m'
+
+# Coordonné point A( ; )
 PasseOrdonneA = 0
 while PasseOrdonneA == 0 :
     try:
-        PointAa = int(input("Point A abscisse : ")) 
+        PointAa = int(input("A(?, ): ")) 
         if PointAa == int(PointAa):
             PasseOrdonneA = 1
     except ValueError:
-        print('IL FAUT ENTRER UN NUMERO')
+        print(Couleur.Rouge + 'IL FAUT ENTRER UN NUMERO' + Couleur.Normal)
 
-if PasseOrdonneA == 1:
-    print("2")
+PasseAbscisseB = 0
+while PasseAbscisseB == 0:
+    try:
+        PointAo = int(input('A( ;?)'))
+        if PointAo == int(PointAo):
+            PasseAbscisseB = 1
+    except ValueError:
+        print(Couleur.Rouge + "IL FAUT UN NUMERO" + Couleur.Normal)
+
+print('A(' + str(PointAa) + ';' + str(PointAo) + ')')
+
+# Coordonné point B( ; )
+PasseOrdonneB = 0
+while PasseOrdonneB == 0:
+    try:
+        PointBa = int(input('B(?; )'))
+        if PointBa == int(PointBa):
+            PasseOrdonneB = 1
+    except ValueError:
+        print(Couleur.Rouge + 'IL FAUT UN NUMERO' + Couleur.Normal)
