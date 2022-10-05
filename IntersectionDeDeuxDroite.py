6f2e9112b3e3d285126c1b23f3e53fc7aee51f9d
Donald2304 <106539903+Donald2304@users.noreply.github.com>
2022-10-05 15:32:59 +0200

    Update IntersectionDeDeuxDroite.py

--- IntersectionDeDeuxDroite.py
+++ IntersectionDeDeuxDroite.py
@@ -4,10 +4,10 @@ afficheResultatD = 0
 afficheResultat = 0
 reductionUser = []
 reductionOrdinateur = []
-listeChiffres = list(range(-10, 10 + 1))
-listeChiffres.remove(0)
-userRepNumerateur2 = 0
-userRepDenominateur2 = 0
+userRepNumerateur2 = None
+userRepDenominateur2 = None
+listeChiffres = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 
 print("ax + b = cx + d")
 print("x(a-c) = d - b")
@@ -25,8 +25,8 @@ print("")
 
 while afficheResultat == 0:
     try:
-        userRepNumerateur = float(input("x = ?/ : "))
-        if userRepNumerateur == float(userRepNumerateur):
+        userRepNumerateur = int(input("x = ?/ : "))
+        if userRepNumerateur == int(userRepNumerateur):
             afficheResultatN = 1
     except ValueError:
         print("Ce n'est pas un nombre")
@@ -35,18 +35,29 @@ while afficheResultat == 0:
     while afficheResultat == 0: 
         if afficheResultatN == 1:
             try:
-                userRepDenominateur = float(input("x = " + str(userRepNumerateur) + " /? : "))
-                if userRepDenominateur == float(userRepDenominateur):
+                userRepDenominateur = int(input("x = " + str(userRepNumerateur) + " /? : "))
+                if userRepDenominateur == int(userRepDenominateur):
                     afficheResultat = 1
             except ValueError:
                 print("Ce n'est pas un nombre")
 print("Votre reponse : x = " + str(userRepNumerateur) + " / " + str(userRepDenominateur))
 
+N = userRepNumerateur / (d-b)
+D = userRepDenominateur / (ax-c) 
+if N == D:
+    userRepNumerateur2 = userRepNumerateur
+    userRepDenominateur2 = userRepDenominateur
 
+    userRepNumerateur2 /= N
+    userRepDenominateur2 /= D
 
-if d-b == userRepNumerateur and ax-c == userRepDenominateur:
+if d-b == userRepNumerateur and ax-c == userRepDenominateur or d-b == userRepNumerateur2 and ax-c == userRepDenominateur2:
     print("Votre reponse est juste")
-
+    if d-b == userRepNumerateur2 and ax-c == userRepDenominateur2:
+        print("Mais elle peut être simplifier")
+        print(str(userRepNumerateur2/N) + " / " + str(userRepDenominateur2/D))
+    else:
+        pass
 else:
     for a in listeChiffres:
         if (d-b) % a == 0 and (ax-c) % a == 0:
