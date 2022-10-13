4438182bebaecf0f763f48221e0bf7624703525f
Donald2304 <106539903+Donald2304@users.noreply.github.com>
2022-10-13 18:20:35 +0200

    Update CoucouJéréme.py

--- CoucouJéréme.py
+++ CoucouJéréme.py
@@ -1,14 +1,30 @@
 import time
 a = 0 
+espace = ""
+nbEspace = 0
 while True:
     if a == 0:
-        time.sleep(0.35)
+        espace = ""
+        nbEspace = 0
+        time.sleep(0.4)
+        while nbEspace < 100:
+            print("")
+            espace += " "        
+            nbEspace += 1
         print(" O/")
         print("/|")
         print("/ \ ")
+
         a = 1
+
     if a == 1:
-        time.sleep(0.35)
+        espace = ""
+        nbEspace = 0
+        time.sleep(0.4)
+        while nbEspace < 100:
+            print("")
+            espace += " "
+            nbEspace += 1
         print(" O__")
         print("/|")
         print("/ \ ")     
